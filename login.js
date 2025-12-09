a5d4ec5c76cd10148a94c316893bcffadea28a46
sushantkadam <sushant.kadam27@gmail.com>
2025-12-09 23:09:28 +0530

    pipelinefile

--- login.js
+++ login.js
@@ -1,17 +1,11 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from 'k6/http'
 
 export const options = {
-  vus: 5,           // virtual users
-  duration: '30s',  // run for 30 seconds
-  thresholds: {
-    'http_req_failed': ['rate<0.01'],          // <1% failed requests
-    'http_req_duration': ['p(95)<500']        // 95% requests < 500ms
-  }
-};
+  vus: 10,
+  iterations: '20'
+} 
 
+  
 export default function () {
-  const res = http.get('https://agreeable-meadow-0eb877f1e.6.azurestaticapps.net/');
-  check(res, { 'status was 200': (r) => r.status === 200 });
-  sleep(1);
+  http.get('https://ailh.ai/login')
 }
