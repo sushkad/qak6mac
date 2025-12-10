0d592b2daa8fca8245498d48f457a35b92c228d4
sushantkadam <sushant.kadam27@gmail.com>
2025-12-10 23:23:15 +0530

    update loginail

--- login.js
+++ login.js
@@ -1,11 +1,24 @@
 import http from 'k6/http'
 
 export const options = {
-  vus: 10,
-  iterations: '20'
-} 
+  vus: 5,
+  iterations: 10
+}
 
-  
 export default function () {
-  http.get('https://ailh.ai/login')
+  const url = 'https://pipeline-dev-b2bygkfbaaa0eab8.westcentralus-01.azurewebsites.net/v1/users/login'
+  
+  const payload = JSON.stringify({
+    email: 'demo@hog.com',
+    password: 'DemoHog@123'
+  })
+  
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  const response = http.post(url, payload, params)
+  console.log(`Status: ${response.status}`)
 }
